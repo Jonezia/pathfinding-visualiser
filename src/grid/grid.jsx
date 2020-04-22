cb1b0fa9660fe63ff17dd8304aafb0c9d5dd8097
Jonezia <adamj6636@gmail.com>
2020-04-22 19:30:53 +0800

    weights, animations, fixed faulty node state

--- src/grid/grid.jsx
+++ src/grid/grid.jsx
@@ -10,28 +10,32 @@ export default function Grid(props) {
     for (let row=0; row<24; row++) {
         let temprow = []
         for (let col=0; col<52; col++) {
-            temprow.push(false)
+        // Using arrays to store node data instead of objects due to speed.
+        // Node = [state,weight]
+        // state = 0: default, 1: wall, 2: explored, 3: fringe
+        //weight = integer 0-5, how much "effort" it takes to expand a node
+            temprow.push([0,0])
         }
         tempGrid.push(temprow)
     }
+
+    // Thus, the structure of a grid is [ row: [ node: [ state: 0-3, weight: 0-5] ]
 
     let modelGrid = tempGrid.slice()
 
     let [myGrid,setMyGrid] = useState(tempGrid)
 
     const clearTerrain = () => {
-        // let newGrid = myGrid.slice()
-        // for (let row=0; row<newGrid.length; row++) {
-        //     for (let col=0; col<row.length; col++) {
-        //         if (newGrid[row][col]) {
-        //             newGrid[row][col] = !newGrid[row][col]
-        //         }
-        //     }
-        // }
-        // setMyGrid(newGrid)
-        // console.log("clearterrain pressed")
-        console.log(modelGrid)
-        setMyGrid([[false,false,true,true,true]])
+        let newGrid = myGrid.slice()
+        for (let row=0; row<newGrid.length; row++) {
+            for (let col=0; col<row.length; col++) {
+                if (newGrid[row][col][0] === 1) {
+                    newGrid[row][col][0] = 0
+                }
+            }
+        }
+        modelGrid = newGrid
+        setMyGrid(modelGrid)
     }
 
     props.setClickClearTerrain(clearTerrain)
@@ -42,11 +46,14 @@ export default function Grid(props) {
     let end = [10,35]
 
     let mouseIsPressed = false
+    let running = false
+    let delay = 100
+    let date = new Date()
 
     const toggleWall = (row,col) => {
         if ((mouseIsPressed) && !((row === start[0]) && (col === start[1])) &&
         !((row === end[0]) && (col === end[1]))) {
-            modelGrid[row][col] = !modelGrid[row][col]
+            modelGrid[row][col][0] = !modelGrid[row][col][0]
             return true
         } else {
             return false
@@ -66,18 +73,49 @@ export default function Grid(props) {
         mouseIsPressed = false
     }
 
+    const animate = (animation) => {
+        // let frames = generateFrames(algorithm, myArray)
+        let frames = [
+            [ [[0,0],[0,0],[1,0]],
+              [[0,0],[0,0],[1,0]] ],
+            [ [[1,0],[1,0],[0,0]],
+              [[0,0],[0,0]] ],
+            [ [[1,0]],
+              [[0,0]]]
+        ]
+        let runAnimations = () => {
+            if (running) {
+            if (i === frames.length) {
+                running = false
+                clearTimeout(timer);
+            } else {
+                setMyGrid(frames[i]);
+                i += 1
+                timer = setTimeout(runAnimations, delay)
+            }
+        }}
+        running = true
+        let timer = setTimeout(runAnimations, delay)
+        let i = 0
+    };
+
+    // date.getTime is appended to the key of each node, to change state
+    // between re-renders, as otherwise the state of the node is preserved
+    // due to states being used for both the grid and the nodes
+
     return(
         <div id="drawArea">
         <svg id="fullGrid">
             {myGrid.map((rowvals,row) => {
-                return rowvals.map((wall,column) => {
+                return rowvals.map((node,column) => {
                     return <Node nodeSize={props.nodeSize}
                     row={row}
                     column={column}
-                    key={row + "." + column}
-                    initialColor={(wall === true) ? "black" : 
-                        (row === start[0] && column === start[1]) ? "red" :
-                        (row === end[0] && column === end[1]) ? "purple" : "white"}
+                    key={`${row}-${column}-${date.getTime()}`}
+                    state={node[0]}
+                    weight={node[1]}
+                    isStart={(row === start[0] && column === start[1])}
+                    isEnd={(row === end[0] && column === end[1])}
                     handleMouseDown={handleMouseDown}
                     handleMouseEnter={handleMouseEnter}
                     handleMouseUp={handleMouseUp}/>})
--- src/grid/node/node.jsx
+++ src/grid/node/node.jsx
@@ -1,16 +1,41 @@
 import React,{useState} from 'react'
 
 export default function Node(props) {
-    let {nodeSize,row,column,initialColor,
+    let {nodeSize,row,column,state,weight,isStart,isEnd,
         handleMouseDown,handleMouseEnter,handleMouseUp} = props
 
-    let [color,setColor] = useState(initialColor);
+    const wallColor = "black"
+    const exploredColor = "green"
+    const fringeColor = "light green"
+    const startColor = "red"
+    const endColor = "purple"
+    const weights = ["rgba(255,255,255,1)", "rgba(204,250,255,1)",
+    "rgba(178,242,255,1)", "rgba(153,229,255,1)", "rgba(127,212,255,1)",
+    "rgba(101,191,255,1)", "rgba(76,165,255,1)", "rgba(50,136,255,1)",
+    "rgba(25,101,240,1)", "rgba(0,63,200,1)"]
+
+    const setInitialColor = () => {
+        if (isStart) {return startColor}
+        else if (isEnd) {return endColor}
+        switch(state) {
+            case 0: return weights[weight];
+            case 1: return wallColor;
+            case 2: return exploredColor;
+            case 3: return fringeColor;
+            default: return weights[0];
+        }
+    }
+
+    let prevColor = weights[0]
+
+    let [color,setColor] = useState(setInitialColor);
 
     const toggleColor = () => {
-        if (color === "white") {
-            setColor("black")
-        } else if (color === "black") {
-            setColor("white")
+        if (color !== wallColor) {
+            prevColor = color
+            setColor(wallColor)
+        } else if (color === wallColor) {
+            setColor(prevColor)
         }
     }
 
