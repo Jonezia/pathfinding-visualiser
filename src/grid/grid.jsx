f599526160c0c9a600cdd799f9e83aabef9926ca
Jonezia <adamj6636@gmail.com>
2020-04-24 02:37:41 +0800

    fixed issue with modelGrid being reset on state update

--- src/grid/grid.jsx
+++ src/grid/grid.jsx
@@ -2,73 +2,56 @@ import React,{useState} from 'react'
 import './grid.css'
 import Node from './node/node'
 
+const newGrid = () => {
+    let tempGrid = []
+    for (let row=0; row<24; row++) {
+        let temprow = []
+        for (let col=0; col<52; col++) {
+        // Using arrays to store node data instead of objects due to speed.
+        // Node = [isWall,isExplored,weight]
+        // isWall, isExplored, boolean
+        //weight = integer 0-9, how much "effort" it takes to expand a node
+            temprow.push([false,false,0])
+        }
+        tempGrid.push(temprow)
+    }
+    return tempGrid
+}
+
+let modelGrid = newGrid()
+
+// Thus, the structure of a grid is
+// [ row: [ node: [ isWall: bool, isExplored: bool, weight: 0-9] ]
+
 export default function Grid(props) {
     let rows = Math.ceil(props.height/props.nodeSize);
     let cols = Math.ceil(props.width/props.nodeSize);
 
-    const newGrid = () => {
-        let tempGrid = []
-        for (let row=0; row<24; row++) {
-            let temprow = []
-            for (let col=0; col<52; col++) {
-            // Using arrays to store node data instead of objects due to speed.
-            // Node = [isWall,isExplored,weight]
-            // isWall, isExplored, boolean
-            //weight = integer 0-9, how much "effort" it takes to expand a node
-                temprow.push([false,false,0])
-            }
-            tempGrid.push(temprow)
-        }
-        return tempGrid
-    }
-
-    // Thus, the structure of a grid is
-    // [ row: [ node: [ isWall: bool, isExplored: bool, weight: 0-9] ]
-
-    let modelGrid = newGrid()
-
-    modelGrid = [
-        [[false,false,0],[false,false,1],[false,false,2],[false,false,3],[false,false,4],
-        [false,false,5],[false,false,6],[false,false,7],[false,false,8],[false,false,9]],
-        [[true,true,0],[true,false,0],[false,true,0],[false,false,0]],
-        [[true,true,5],[true,false,5],[false,true,5],[false,false,5]]
-    ]
-
     let [myGrid,setMyGrid] = useState(modelGrid)
 
-    const updateModelGridClearTerrain = (grid) => {
-        for (let row=0; row<grid.length; row++) {
-            for (let col=0; col<grid[row].length; col++) {
-                if (grid[row][col][0]) {
-                    grid[row][col][0] = !grid[row][col]
+    const clearTerrain = () => {
+        for (let row=0; row<modelGrid.length; row++) {
+            for (let col=0; col<modelGrid[row].length; col++) {
+                if (modelGrid[row][col][0]) {
+                    modelGrid[row][col][0] = !modelGrid[row][col]
                 }
-                grid[row][col][2] = 0
+                modelGrid[row][col][2] = 0
             }
         }
-        return grid
-    }
-
-    const clearTerrain = () => {
-        let newGrid = updateModelGridClearTerrain(modelGrid)
-        setMyGrid(newGrid)
+        setMyGrid(modelGrid.slice())
     }
 
     props.setClickClearTerrain(clearTerrain)
 
-    const updateModelGridClearPath = (grid) => {
-        for (let row=0; row<grid.length; row++) {
-            for (let col=0; col<grid[row].length; col++) {
-                if (grid[row][col][1] === true) {
-                    grid[row][col][1] = false
+    const clearPath = () => {
+        for (let row=0; row<modelGrid.length; row++) {
+            for (let col=0; col<modelGrid[row].length; col++) {
+                if (modelGrid[row][col][1] === true) {
+                    modelGrid[row][col][1] = false
                 }
             }
         }
-        return grid
-    }
-
-    const clearPath = () => {
-        let newGrid = updateModelGridClearPath(modelGrid)
-        setMyGrid(newGrid)
+        setMyGrid(modelGrid.slice())
     }
 
     props.setClickClearPath(clearPath)
@@ -88,7 +71,6 @@ export default function Grid(props) {
         if ((leftMouseIsPressed) && !((row === start[0]) && (col === start[1])) &&
         !((row === end[0]) && (col === end[1]))) {
             modelGrid[row][col][0] = !modelGrid[row][col][0]
-            console.log(modelGrid)
             return true
         } else {
             return false
@@ -100,7 +82,6 @@ export default function Grid(props) {
         !((row === end[0]) && (col === end[1])) && (modelGrid[row][col][2] < 9)
         && !(modelGrid[row][col][0])) {
             modelGrid[row][col][2] += 1
-            console.log(modelGrid)
             return modelGrid[row][col][2]
         } else {
             return false
